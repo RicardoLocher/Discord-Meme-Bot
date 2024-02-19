c3a1800a78faa01901dbd86b85ad9282ca7a4590
RicardoLocher <ricardo.locher123@gmail.com>
2024-02-19 12:35:07 +0100

    Can now send real memes from reddit (including coding memes). Can also send multiple ones

--- index.js
+++ index.js
@@ -16,16 +16,49 @@ const client = new Client({
 });
 
 client.on('messageCreate', async(message) => {
+    let words = message.content.toLowerCase().split(" ");
     if (!message.author.bot){
+        if (words.length === 1) {
+            switch (words[0]) {
+                case 'meme':
+                    message.reply('type !meme to query one');
+                    break;
+    
+                case '!meme':
+                    let data = await fetch("https://meme-api.com/gimme/memes").then(response => response.json());
+                    message.channel.send(data.url);
+                    break;
+    
+                case '!codememe':
+                    let data2 = await fetch("https://meme-api.com/gimme/ProgrammerHumor").then(response => response.json());
+                    message.channel.send(data2.url);
+                    break;
+            }
+        }
 
-        switch (message.content.toLowerCase()) {
-            case 'meme':
-                message.reply('type !meme to query one');
-                break;
-            
-            case 'ricardo kanns halt':
-                message.reply('Das Stimmt');
-                break;
+        else if(words.length === 2){
+            if(words[0] === "!meme" && typeof parseInt(words[1]) === 'number'){
+                if(words[1] > 4){
+                    message.reply('The number of memes is too high');
+                }
+                else{
+                    let data3 = await fetch(`https://meme-api.com/gimme/memes/${words[1]}`).then(response => response.json());
+                    for (let i = 0; i < words[1]; i++) {
+                        message.channel.send(data3.memes[i].url);
+                    }
+                }
+            }
+            else if(words[0] === "!codememe" && typeof parseInt(words[1]) === 'number'){
+                if(words[1] > 4){
+                    message.reply('The number of memes is too high');
+                }
+                else{
+                    let data3 = await fetch(`https://meme-api.com/gimme/ProgrammerHumor/${words[1]}`).then(response => response.json());
+                    for (let i = 0; i < words[1]; i++) {
+                        message.channel.send(data3.memes[i].url);
+                    }
+                }
+            }
         }
     };
 });
